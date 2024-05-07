85b1a752fb4c6995c97a9841940d9d0d8849b039
José Carlos de Camargo <jccamargo15@gmail.com>
2024-05-07 00:00:37 -0300

    setup modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  modules: [
+    "@nuxt/tailwindcss",
+    "@nuxt/google-fonts", 
+    "nuxt-primevue"
+  ],
+
+  css: ["primeicons/primeicos.css"],
 })

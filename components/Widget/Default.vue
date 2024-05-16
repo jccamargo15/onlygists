d403bf49127cf17521d0188bfa18cb4d0a7fce35
José Carlos de Camargo <jccamargo15@gmail.com>
2024-05-15 22:41:08 -0300

    feat: WidgetDefault component

---
+++ components/Widget/Default.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title?: string;
+  subtitle?: string;
+}>();
+</script>
+
+<template>
+  <div class="rounded-lg p-5 bg-white overflow-x-auto">
+    <div class="flex justify-between items-center" v-if="props.title">
+      <div class="flex flex-col gap-1">
+        <h3 class="font-[Inter] text-lg text-gray-700 font-bold text-left">
+          {{ props.title }}
+        </h3>
+        <p
+          v-if="props.subtitle" 
+          class="font-[Inter] text-sm text-gray-500 text-left"
+        >
+          {{ props.subtitle }}
+        </p>
+      </div>
+
+      <slot name="header-actions" />
+    </div>
+
+    <div class="py-2">
+      <slot />
+    </div>
+  </div>
+</template> 
